0d7889204c627ebb0d6abe258b9e0c53f195ceb4
C9999 <car_soad@hotmail.com>
2020-01-28 19:20:55 -0300

    workshop parte 1

--- FlappyBrids/GameScene.swift
+++ FlappyBrids/GameScene.swift
@@ -11,79 +11,43 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var sprite: SKSpriteNode
+    
+    override init(size: CGSize) {
+        sprite = SKSpriteNode(imageNamed: "bird-01")
+        super.init(size:size)
+        
+//        sprite.anchorPoint = CGPoint(x: 0, y: 0)
+        sprite.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        sprite.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width / 2)
+        
+        sprite.setScale(3)
+        addChild(sprite)
+        loadAnimation()
+        
+        scene?.physicsWorld.gravity = CGVector(dx: 0, dy: 1)
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError( "init(coder:) has not been implemented")
+    }
     
     override func didMove(to view: SKView) {
+        super.didMove(to: view)
+    }
+    
+    private func loadAnimation(){
+        let texturesNames = ["bird-01", "bird-02", "bird-03", "bird-04"]
+        var textures: [SKTexture] = []
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        for name in texturesNames {
+            let texture = SKTexture(imageNamed: name)
+            texture.filteringMode = .nearest
+            textures.append(texture)
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        let animation = SKAction.animate(with: textures, timePerFrame: 0.2)
+        let repeatForever = SKAction.repeatForever(animation)
+        sprite.run(repeatForever)
     }
 }
